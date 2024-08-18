e3b0831282dde8087e7bc369d27290adbe51b4f3
Daniel Vielma <124645048+Lilsup99@users.noreply.github.com>
2024-08-18 05:04:13 +0000

    add

--- eda.py
+++ eda.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from styles import style
 intro = """
-# Analisis Exploratorio de Datos Automatico\n
+# Analisis Exploratorio de Datos Automatico
 ### Daniel Vielma Data Scientist\n
 
 Hacer un analisis exploratorio de datos resulta beneficioso. Esto permite observar las características fundamentales de los mismos, comprender la estructura del conjunto de datos, identificar la variable objetivo y explorar posibles técnicas de modelado.\n
